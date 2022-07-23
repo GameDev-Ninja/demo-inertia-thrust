691d6ae4ad75123220f02dc96a1ad610689a3493
VinceDZiree <contact@vincentdesiree.fr>
2022-07-23 12:18:35 +0200

    Making use of drawPolygon()

--- fns/drawShip.js
+++ fns/drawShip.js
@@ -6,27 +6,20 @@
 function drawShip(s, ctx){
    let [p1,p2,p3] = eqTriangleFromCenter(0, 0, s.side)
    ctx.fillStyle = "white"
-   
-   
+
    ctx.translate(s.x, s.y)
    ctx.rotate(s.a * Math.PI / 180)
-   ctx.beginPath()
-   ctx.moveTo(p1.x, p1.y)
-   ctx.lineTo(p2.x, p2.y)
-   ctx.lineTo(p3.x, p3.y)
-   ctx.closePath()
-   ctx.fill()
+   ctx.drawPolygon([p1,p2,p3])
 
    ctx.fillRect(p2.x + s.side/3, p2.y, s.side/3, 4)
 
    if(s.thrust){
       ctx.fillStyle = "red"
-      ctx.beginPath()
-      ctx.moveTo(p1.x, p2.y+s.side /3 +5)
-      ctx.lineTo(p2.x + s.side/3, p2.y +5)
-      ctx.lineTo(p3.x - s.side/3, p2.y +5)
-      ctx.closePath()
-      ctx.fill()
+      ctx.drawPolygon([
+         {x: p1.x, y: p2.y+s.side /3 +5},
+         {x: p2.x + s.side/3, y: p2.y +5},
+         {x: p3.x - s.side/3, y: p2.y +5}
+      ])
    }
    ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
@@ -43,8 +36,9 @@ function eqTriangleFromCenter(x,y,s){
    // https://fr.wikipedia.org/wiki/Triangle_%C3%A9quilat%C3%A9ral#%C3%89l%C3%A9ments_remarquables
 
    let h = s*Math.sqrt(3)/2
-   let p1 = {x: x, y: y - 2/3*h}
-   let p2 = {x: x - s/2, y: y + 1/3*h}
-   let p3 = {x: x + s/2, y: y + 1/3*h}
-   return [p1, p2, p3]
+   return [
+      {x: x, y: y - 2/3*h},
+      {x: x - s/2, y: y + 1/3*h},
+      {x: x + s/2, y: y + 1/3*h}
+   ]
 }
